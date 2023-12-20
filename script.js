808979fb917081948a14328ed20cbf9c5a0c99bc
1Abisai <abisai.esc@gmail.com>
2023-12-20 16:14:29 -0700

    updated

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function big(one) {
-    one.style.height = "30px";
-    one.style.width = "30px";
-}
-
-function normal(li) {
-    li.style.height = "12px"
-    li.style.height = "12px"
+function change(one) {
+    document.querySelector(".active").classList.remove("active")
+    one.classList.add("active") 
 }
